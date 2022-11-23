808e160acade8573ec38910e900ef7ab59f3bf3d
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-23 01:32:01 -0300

    feat(Models): add Car ODM

---
+++ src/Models/MongoODM.ts
@@ -0,0 +1,17 @@
+import { Model, models, Schema, model } from 'mongoose';
+
+export default abstract class MongoODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(modelName: string, schema: Schema) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+}

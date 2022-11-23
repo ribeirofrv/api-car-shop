e7e310f35d02eb979275a179962d16cc943cad30
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-23 00:13:53 -0300

    feat(app): add /cars routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandle';
+import carsRouter from './Routes/Cars';
 
 const app = express();
+
+app.use(express.json());
+app.use(carsRouter);
+app.use(ErrorHandler.handle);
 
 export default app;

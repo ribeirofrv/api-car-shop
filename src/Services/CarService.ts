12b6cb2229b6b192b2b93639d1969c9ef07224a7
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-26 16:57:25 -0300

    feat(Services/Car): adds exception throwing in case of error in getCarByID method

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,28 +1,40 @@
+import { isValidObjectId } from 'mongoose';
+
 import ICar from '../Interfaces/ICar';
 import Car from '../Domains/Car';
 import CarODM from '../Models/CarODM';
+import UnprocessableEntityError from '../Errors/UnprocessableEntityError';
+import NotFoundError from '../Errors/NotFoundError';
 
 export default class CarService {
-  private createCarDomain(car: ICar | null): Car | null {
+  private carODM: CarODM;
+
+  constructor() {
+    this.carODM = new CarODM();
+  }
+
+  private createCarDomain(car: ICar | null): Car | null | undefined {
     if (car) return new Car(car);
     return null;
   }
 
   public async createCar(car: ICar) {
-    const carODM = new CarODM();
-    const newCar = await carODM.create(car);
+    const newCar = await this.carODM.create(car);
     return this.createCarDomain(newCar);
   }
 
   public async getCars() {
-    const carODM = new CarODM();
-    const cars = await carODM.getAll();
+    const cars = await this.carODM.getAll();
     return cars.map((car) => this.createCarDomain(car));
   }
 
   public async getCarById(id: string) {
-    const carODM = new CarODM();
-    const car = await carODM.getById(id);
+    if (!isValidObjectId(id)) throw new UnprocessableEntityError('Invalid mongo id');
+    
+    const car = await this.carODM.getById(id);
+    
+    if (!car) throw new NotFoundError('Car not found');
+    
     return this.createCarDomain(car);
   }
 }

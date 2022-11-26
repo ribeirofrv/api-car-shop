2173de6dad2ea0a48b6397c49409703882ef87f9
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-26 00:09:11 -0300

    feat(Models/MongoODM): adds methods for searching and reading

--- src/Models/MongoODM.ts
+++ src/Models/MongoODM.ts
@@ -14,4 +14,12 @@ export default abstract class MongoODM<T> {
   public async create(obj: T): Promise<T> {
     return this.model.create({ ...obj });
   }
+
+  public async getAll(): Promise<T[]> {
+    return this.model.find();
+  }
+
+  public async getById(_id: string): Promise<T | null> {
+    return this.model.findById({ _id });
+  }
 }

bf57ec9e3ea2d43a7a5fed787127a87aa3605a5e
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-26 00:06:42 -0300

    feat(Routes): add GET /cars && GET /cars/:id endpoints

--- src/Routes/Cars.ts
+++ src/Routes/Cars.ts
@@ -4,6 +4,10 @@ import CarsController from '../Controllers/CarsController';
 const carsRouter = Router();
 
 carsRouter.post('/cars', (req, res, next) =>
-  new CarsController(req, res, next).createCar());
+  new CarsController(req, res, next).createCar())
+  .get('/cars', (req, res, next) =>
+    new CarsController(req, res, next).getCars())
+  .get('/cars/:id', (req, res, next) =>
+    new CarsController(req, res, next).getCarById());
 
 export default carsRouter;

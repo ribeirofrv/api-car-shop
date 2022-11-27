8408c9d89d42194f04b1094e6e5104edab37bc59
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-26 23:39:56 -0300

    feat: add route for motorcycles

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandle';
 import carsRouter from './Routes/Cars';
+import motorcycleRouter from './Routes/Motorcycle';
 
 const app = express();
 
 app.use(express.json());
 app.use(carsRouter);
+app.use(motorcycleRouter);
 app.use(ErrorHandler.handle);
 
 export default app;

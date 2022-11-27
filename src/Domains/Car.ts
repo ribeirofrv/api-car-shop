ac34db18afc0ff4fc87b2d243676e364d81f0381
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-26 23:14:10 -0300

    refactor(Domains/Car): tweaks the Car domain to extend from Vehicle

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,39 +1,13 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean | undefined;
-  protected buyValue: number;
 
   constructor(car: ICar) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.status = car.status || false;
-    this.buyValue = car.buyValue;
+    super(car);
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
   }
-
-  // public get doorsQty(): number {
-  //   return this._doorsQty;
-  // }
-
-  // public set doorsQty(doorsQty: number) {
-  //   this._doorsQty = doorsQty;
-  // }
-
-  // public get seatsQty(): number {
-  //   return this._seatsQty;
-  // }
-
-  // public set seatsQty(seatsQty: number) {
-  //   this._seatsQty = seatsQty;
-  // }
 }
